64d3f40db8c0d7139666517eac125aa6d45e4472
Wakanda <czhpeter008@gmail.com>
2020-04-09 17:54:41 +0800

    创建用户模块：创建注册用户趋势echart、get_added_user函数

--- user/urls.py
+++ user/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path("get_all_user/", views.get_all_user,name='get_all_user'),
     path("add_user/", views.add_user,name='edit_user'),
     path("edit_user/", views.edit_user,name='edit_user'),
+    path("render_added_echart/", views.render_added_echart,name='render_added_echart'),
+    path("render_num_map/", views.render_num_map,name='render_num_map'),
+    path("get_added_num/", views.get_added_num,name='get_added_num'),
+    path("get_map_data/", views.get_map_data,name='get_map_data'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,7 +1,8 @@
-import json
+import json, MySQLdb, time
+from datetime import datetime
 
 from django.core.paginator import Paginator
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
@@ -17,7 +18,6 @@ def get_all_user(request):
     rows = request.GET.get('rows')
     page = request.GET.get('page')
     user_list = TUser.objects.all().order_by('user_id')
-    print(user_list)
     all_page = Paginator(user_list, rows)
 
     # 处理最后删除当页最后一条数据
@@ -50,7 +50,7 @@ def get_all_user(request):
 @csrf_exempt
 def add_user(request):
     """
-    轮播图表添加一行数据
+    用户数据表添加一行数据
     :param request: 需要添加的数据
     :return:
     """
@@ -79,7 +79,7 @@ def add_user(request):
 @csrf_exempt
 def edit_user(request):
     """
-    修改、删除轮播图表中的数据
+    修改、删除用户数据表中的数据
     :param request: 当行数据、修改或者删除
     :return:
     """
@@ -92,7 +92,7 @@ def edit_user(request):
         nickname = request.POST.get('nickname')
         address = request.POST.get('address')
         status = request.POST.get('status')
-        print(id, username,nickname,address, status)
+        print(id, username, nickname, address, status)
         user = TUser.objects.get(user_id=id)
         user.username = username
         user.nickname = nickname
@@ -106,3 +106,95 @@ def edit_user(request):
         user = TUser.objects.get(user_id=id)
         user.delete()
         return HttpResponse('删除成功')
+
+
+def render_added_echart(request):
+    """
+    渲染新增用户数量ehcart网页
+    """
+    return render(request, 'added_user_echart.html')
+
+
+def render_num_map(request):
+    """
+    渲染用户数量地图
+    """
+    return render(request, 'user_num_map.html')
+
+
+def get_added_num(request):
+    """
+    数据库中获取一周内新注册用户数量
+    :param request:
+    :return: 返回echart数据
+    """
+    x = ['2020-04-02',
+         '2020-04-03',
+         '2020-04-04',
+         '2020-04-05',
+         '2020-04-06',
+         '2020-04-07',
+         '2020-04-08']
+    y = [0, 0, 0, 0, 0, 0, 0]
+    users = TUser.objects.filter(register_time__range=('2020-04-02', '2020-04-09'))
+    print(users)
+    for user in users:
+        date = user.register_time.strftime("%Y-%m-%d")
+        print(date)
+        for i in x:
+            if i == date:
+                y[x.index(i)] += 1
+    print(y)
+    data = {
+        'x': x,
+        'y': y,
+    }
+    return JsonResponse(data)
+
+
+def get_map_data(request):
+    """
+    数据库中更具地区筛选各个省的人数
+    :param request: 
+    :return: 
+    """
+    data = [
+        {'name': '北京', 'value': 0},
+        {'name': '天津', 'value': 0},
+        {'name': '上海', 'value': 0},
+        {'name': '重庆', 'value': 0},
+        {'name': '河北', 'value': 0},
+        {'name': '河南', 'value': 0},
+        {'name': '云南', 'value': 0},
+        {'name': '辽宁', 'value': 0},
+        {'name': '湖南', 'value': 0},
+        {'name': '安徽', 'value': 0},
+        {'name': '⼭东', 'value': 0},
+        {'name': '新疆', 'value': 0},
+        {'name': '江苏', 'value': 0},
+        {'name': '浙江', 'value': 0},
+        {'name': '江⻄', 'value': 0},
+        {'name': '湖北', 'value': 0},
+        {'name': '⼴⻄', 'value': 0},
+        {'name': '⽢肃', 'value': 0},
+        {'name': '⼭⻄', 'value': 0},
+        {'name': '陕⻄', 'value': 0},
+        {'name': '吉林', 'value': 0},
+        {'name': '福建', 'value': 0},
+        {'name': '贵州', 'value': 0},
+        {'name': '⼴东', 'value': 0},
+        {'name': '⻘海', 'value': 0},
+        {'name': '⻄藏', 'value': 0},
+        {'name': '四川', 'value': 0},
+        {'name': '宁夏', 'value': 0},
+        {'name': '海南', 'value': 0},
+        {'name': '台湾', 'value': 0},
+        {'name': '⾹港', 'value': 0},
+        # {'name': '澳⻔', 'value': Math.round(Math.random() * 1000)}
+    ]
+    for i in data:
+        if TUser.objects.filter(address=i['name']):
+            i['value'] += 1
+    print(data)
+
+
